b508ea1f79c9d613abca62282a8d3533409ea924
Luis Stasi <stasi93@libero.it>
2026-03-13 10:52:52 +0000

    feat: add pip-boy boot animation screen with terminal style startup

---
+++ app/screens/boot_screen.py
@@ -0,0 +1,63 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+from app.core.theme import theme
+
+
+class BootScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.boot_lines = [
+            "VAULT-TEC INDUSTRIES",
+            "",
+            "INITIALIZING PIP-BOY 3000 MK IV...",
+            "LOADING SYSTEM FILES...",
+            "CHECKING RADIATION LEVELS...",
+            "SYNCING VAULT DATABASE...",
+            "",
+            "WELCOME VAULT DWELLER",
+        ]
+
+        self.current_line = 0
+
+        self.label = Label(
+            text="",
+            font_name=theme.font,
+            font_size=22,
+            color=theme.text,
+            halign="center",
+            valign="top",
+        )
+
+        self.label.bind(size=self.label.setter("text_size"))
+
+        self.add_widget(self.label)
+
+        Clock.schedule_once(self.start_boot, 0.5)
+
+    def start_boot(self, dt):
+        Clock.schedule_interval(self.print_line, 0.6)
+
+    def print_line(self, dt):
+
+        if self.current_line >= len(self.boot_lines):
+            Clock.schedule_once(self.finish_boot, 1)
+            return False
+
+        line = self.boot_lines[self.current_line]
+
+        self.label.text += line + "\n"
+
+        self.current_line += 1
+
+    def finish_boot(self, dt):
+        root = self.manager.parent
+        navbar = root.children[1]
+
+        navbar.opacity = 1
+        navbar.disabled = False
+
+        self.manager.current = "stat"
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from app.screens.inventory_screen import InventoryScreen
 from app.screens.data_screen import DataScreen
 from app.screens.map_screen import MapScreen
 from app.screens.radio_screen import RadioScreen
+from app.screens.boot_screen import BootScreen
 
 from app.widgets.navbar import NavBar
 from app.widgets.ctr_overlay import CRTOverlay
@@ -33,16 +34,21 @@ class PipBoyApp(App):
         # SCREEN MANAGER
         screen_manager = ScreenManager(size_hint=(1, 1))
 
+        screen_manager.add_widget(BootScreen(name="boot"))
         screen_manager.add_widget(StatScreen(name="stat"))
         screen_manager.add_widget(InventoryScreen(name="inv"))
         screen_manager.add_widget(DataScreen(name="data"))
         screen_manager.add_widget(MapScreen(name="map"))
         screen_manager.add_widget(RadioScreen(name="radio"))
 
+        screen_manager.current = "boot"
+
         # NAVBAR
         navbar = NavBar(screen_manager)
         navbar.size_hint_y = None
         navbar.height = 60
+        navbar.opacity = 0
+        navbar.disabled = True
 
         # BUILD MAIN LAYOUT
         main_layout.add_widget(navbar)
